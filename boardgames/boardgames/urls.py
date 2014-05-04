36916f3c06963e9e5ba1014954b92bddae584294
Jan Milosh <janmilosh@gmail.com>
2014-05-04 13:06:16 -0400

    Adds the tictactoe app and models

--- boardgames/boardgames/urls.py
+++ boardgames/boardgames/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
-from .views import HelloWorldView
+from views import HelloWorldView
 
 admin.autodiscover()
 
--- boardgames/boardgames/views.py
+++ boardgames/boardgames/views.py
@@ -1,6 +1,8 @@
-from django.views.generic.base import View
-from django.http import HttpResponse
+from django.views.generic.base import TemplateView
+from datetime import date
 
-class HelloWorldView(View):
-    def get(self, request):
-        return HttpResponse("Hello, World!")
+class HelloWorldView(TemplateView):
+    template_name = "helloworld.html"
+
+    def get_context_data(self, **kwargs):
+        return {'date': date.today()}
