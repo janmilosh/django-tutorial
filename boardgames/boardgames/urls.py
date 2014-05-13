0ded7b6a018241e0d2ad0cf46d347fee47466232
Jan Milosh <janmilosh@gmail.com>
2014-05-12 22:10:32 -0400

    Currently broken, but adds home view and bootstrap file links

--- boardgames/boardgames/urls.py
+++ boardgames/boardgames/urls.py
@@ -1,23 +1,20 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
-from views import HelloWorldView
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'main.views.home', name='boardgames_home'),  
+    url(r'^$', 'main.views.home', name = 'boardgames_home'),  
 )
 
 urlpatterns += patterns(
     'django.contrib.auth.views',
 
     url(r'^login/$', 'login',
-        {'template_name': 'login.html'},
-        name='boardgames_login'),
+        {'template_name': 'login.html'}, name='boardgames_login'),
 
     url(r'^logout/$', 'logout',
-        {'next_page': 'boardgames_home'},
-        name='boardgames_logout'),
+        {'next_page': 'boardgames_home'}, name='boardgames_logout'),
 )
--- boardgames/main/views.py
+++ boardgames/main/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic.base import TemplateView
-from datetime import date
 
 class Home(TemplateView):
     template_name = "home.html"
